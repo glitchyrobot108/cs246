c48ada16208f623923c755573088c4c5f5d071c0
minjae970 <par15033@byui.edu>
2021-01-30 19:30:53 -0700

    .

--- HelloFromCasey/src/casey/Main.java
+++ HelloFromCasey/src/casey/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Casey");
         System.out.println("Hello Casey, my name is Flor.");
-        System.out.println("Hwllo Casey, my name is James");
+        System.out.println("Hello Casey, my name is James");
     }
 }
 
